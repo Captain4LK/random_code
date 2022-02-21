9350804116b32d3d95c6f285cc06bb253d81ee2d
Captain4LK <captain4lk@tutanota.com>
2022-02-21 21:39:56 +0100

    First working version of markov

---
+++ markov/bestoff.md
@@ -0,0 +1,5 @@
+Edgar Allan Poe:
+
+> Ah, broken by poets -- as the twins of Pallas just above my chamber door-
+
+> For the moon never beams without a water that flows,
--- markov/markov_text.c
+++ markov/markov_text.c
@@ -1,5 +1,5 @@
 /*
-markov sentence
+markov sentence generator
 
 Written in 2022 by Lukas Holzbeierlein (Captain4LK) email: captain4lk [at] tutanota [dot] com
 
@@ -25,6 +25,10 @@ You should have received a copy of the CC0 Public Domain Dedication along with t
    ((++(I))<argc?argv[(I)]:NULL)
 
 #define ORDER 3
+#define MORE_RANDOM 1
+#define WEIGHTED_RANDOM 1
+
+#define FNV_32_PRIME ((uint32_t)0x01000193)
 //-------------------------------------
 
 //Typedefs
@@ -35,34 +39,34 @@ typedef struct Count Count;
 typedef struct
 {
    char **data;
-   unsigned data_used;
-   unsigned data_size;
+   uint32_t data_used;
+   uint32_t data_size;
 }String_array;
 
 typedef struct
 {
    uint32_t *data;
-   unsigned data_used;
-   unsigned data_size;
+   uint32_t data_used;
+   uint32_t data_size;
 }Uint32_array;
 
 typedef struct
 {
    Context *data;
-   unsigned data_used;
-   unsigned data_size;
+   uint32_t data_used;
+   uint32_t data_size;
 }
 Context_array;
 typedef struct
 {
    Count *data;
-   unsigned data_used;
-   unsigned data_size;
+   uint32_t data_used;
+   uint32_t data_size;
 }Count_array;
 
 typedef struct
 {
-   String_array words;
+   String_array words[256];
    Uint32_array starting_words;
    Context_array contexts[256];
 }Markov_model;
@@ -90,19 +94,23 @@ static void print_help(char **argv);
 static char *file_read(const char *path);
 
 static void uint32_array_add(Uint32_array *array, uint32_t num);
+static void uint32_array_free(Uint32_array *array);
+
 static void string_array_add(String_array *array, const char *str);
-static void string_array_add_words(String_array *array, const char *str);
 static uint32_t string_array_add_unique(String_array *array, const char *str);
 static Context *context_find_or_create(Context_array *array, uint32_t context[ORDER], uint32_t context_size);
 static Context *context_find(const Context_array *array, uint32_t context[ORDER], uint32_t context_size);
 static void count_array_add(Count_array *array, uint32_t word);
 
-static Markov_model *markov_model_create(String_array *words, Uint32_array *starting_words);
+static Markov_model *markov_model_create();
 static void markov_model_generate(const Markov_model *model);
+static uint32_t markov_model_add_word(Markov_model *model, const char *word);
+static void markov_model_add_start(Markov_model *model, uint32_t start);
+static const char *markov_model_get_word(const Markov_model *model, uint32_t word); 
+static void markov_model_stats(const Markov_model *model);
 
-static uint8_t xor_string(const char *str);
-
-static char *HLH_strtok(char *s, const char *sep, char **context);
+static uint32_t fnv32a(const char *str);
+static char *HLH_strtok(char *s, const char *sep, char **context); //strtok, but with a external pointer instead of a static variable for tracking internal state
 //-------------------------------------
 
 //Function implementations
@@ -111,6 +119,8 @@ int main(int argc, char **argv)
 {
    //Parse cmd arguments
    const char *path = NULL;
+   int print_stats = 0;
+   int num_outputs = 1;
    for(int i = 1;i<argc;i++)
    {
       if(strcmp(argv[i],"--help")==0||
@@ -120,6 +130,10 @@ int main(int argc, char **argv)
          print_help(argv);
       else if(strcmp(argv[i],"-i")==0)
          path = READ_ARG(i);
+      else if(strcmp(argv[i],"-stats")==0)
+         print_stats = 1;
+      else if(strcmp(argv[i],"-gen")==0)
+         num_outputs = atoi(READ_ARG(i));
    }
 
    if(path==NULL)
@@ -130,26 +144,29 @@ int main(int argc, char **argv)
 
    srand(time(NULL));
 
-   //Read file line by line
+   //Read file entry by entry
+   //Entries are sepperated by a '#' symbol followed by a newline
    String_array lines = {0};
    char *file = file_read(path); //TODO: don't do this, won't work for gigantic files
-   const char *token = "\n"; 
-   char *context = NULL;
-   char *line = HLH_strtok(file,token,&context);
-   while(line!=NULL)
+   char *str = file;
+   char *ptr = file;
+   while(*ptr++!='\0')
    {
-      string_array_add(&lines,line);
-
-      line = HLH_strtok(NULL,token,&context);
+      if(*ptr=='#'&&*(ptr+1)=='\n')
+      {
+         *ptr = '\0';
+         string_array_add(&lines,str);
+         ptr+=2;
+         str = ptr;
+      }
    }
    free(file);
 
    //Collect words and convert sentences to integers
-   String_array words = {0};
-   Uint32_array starting_words = {0};
+   Markov_model *model = markov_model_create();
    Uint32_array *sentences = malloc(sizeof(*sentences)*lines.data_used);
    memset(sentences,0,sizeof(*sentences)*lines.data_used);
-   for(int i = 0;i<lines.data_used;i++)
+   for(uint32_t i = 0;i<lines.data_used;i++)
    {
       char *str_line = malloc(sizeof(*str_line)*(strlen(lines.data[i])+1));
       strcpy(str_line,lines.data[i]);
@@ -160,12 +177,12 @@ int main(int argc, char **argv)
       int first = 1;
       while(word!=NULL)
       {
-         uint32_t word_index = string_array_add_unique(&words,word);
+         uint32_t word_index = markov_model_add_word(model,word);
          uint32_array_add(&sentences[i],word_index);
          if(first)
          {
             first = 0;
-            uint32_array_add(&starting_words,word_index);
+            markov_model_add_start(model,word_index);
          }
 
          word = HLH_strtok(NULL,token,&context);
@@ -173,15 +190,12 @@ int main(int argc, char **argv)
 
       free(str_line);
    }
-   
-   printf("Finished parsing '%s'\n%d inputs\n%d unique words\n",path,lines.data_used,words.data_used);
    free(lines.data);
 
    //Create markov chain
-   Markov_model *model = markov_model_create(&words,&starting_words);
-   for(int i = 0;i<lines.data_used;i++)
+   for(uint32_t i = 0;i<lines.data_used;i++)
    {
-      for(int j = 1;j<sentences[i].data_used;j++)
+      for(uint32_t j = 1;j<sentences[i].data_used;j++)
       {
          uint32_t context[ORDER] = {0};
          uint32_t event = sentences[i].data[j];
@@ -189,7 +203,7 @@ int main(int argc, char **argv)
          uint8_t xor = 0;
          for(int m = 1;m<=ORDER;m++)
          {
-            if(j-m<0)
+            if((int)j-m<0)
                break;
 
             context[m-1] = sentences[i].data[j-m];
@@ -202,10 +216,20 @@ int main(int argc, char **argv)
             count_array_add(&model_context->counts,event);
          }
       }
-   }
 
-   for(int i = 0;i<32;i++)
+      uint32_array_free(&sentences[i]);
+   }
+   free(sentences);
+
+   if(print_stats)
+      markov_model_stats(model);
+
+   //Generate sentences
+   for(int i = 0;i<num_outputs;i++)
+   {
       markov_model_generate(model);
+      puts("-------------------------------------------------------------------------------------");
+   }
 
    return 0;
 }
@@ -213,8 +237,10 @@ int main(int argc, char **argv)
 static void print_help(char **argv)
 {
    printf("%s usage:\n"
-          "%s -i filename\n"
-          "   -i    file to read input from\n",
+          "%s -i filename [-stats] [-gen NUM]\n"
+          "   -i     file to read input from\n"
+          "   -stats print markov model stats\n"
+          "   -gen   number of outputs to generate\n",
          argv[0],argv[0]);
 }
 
@@ -251,6 +277,17 @@ static void uint32_array_add(Uint32_array *array, uint32_t num)
    }
 }
 
+static void uint32_array_free(Uint32_array *array)
+{
+   if(array==NULL||array->data==NULL)
+      return;
+
+   free(array->data);
+   array->data = NULL;
+   array->data_used = 0;
+   array->data_size = 0;
+}
+
 static void string_array_add(String_array *array, const char *str)
 {
    if(array->data==NULL)
@@ -270,24 +307,6 @@ static void string_array_add(String_array *array, const char *str)
       array->data_size+=16;
       array->data = realloc(array->data,sizeof(*array->data)*array->data_size);
    }
-}
-
-static void string_array_add_words(String_array *array, const char *str)
-{
-   char *str_line = malloc(sizeof(*str_line)*(strlen(str)+1));
-   strcpy(str_line,str);
-
-   const char *token = " ";
-   char *context = NULL;
-   char *word = HLH_strtok(str_line,token,&context);
-   while(word!=NULL)
-   {
-      string_array_add_unique(array,word);
-
-      word = HLH_strtok(NULL,token,&context);
-   }
-
-   free(str_line);
 }
 
 static uint32_t string_array_add_unique(String_array *array, const char *str)
@@ -393,22 +412,51 @@ static void count_array_add(Count_array *array, uint32_t word)
    }
 }
 
-static uint8_t xor_string(const char *str)
-{
-   uint8_t result = 0;
-   for(;*str!='\0';str++)
-      result^=(uint8_t)*str;
-
-   return result;
-}
-
-static Markov_model *markov_model_create(String_array *words, Uint32_array *starting_words)
+static Markov_model *markov_model_create()
 {
    Markov_model *model = malloc(sizeof(*model));
    memset(model,0,sizeof(*model));
-   model->words = *words;
-   model->starting_words = *starting_words;
    return model;
+}
+
+static uint32_t markov_model_add_word(Markov_model *model, const char *word)
+{
+   uint8_t index = fnv32a(word)&255;
+   return string_array_add_unique(&model->words[index],word)|(index<<24);
+}
+
+static void markov_model_add_start(Markov_model *model, uint32_t start)
+{
+   uint32_array_add(&model->starting_words,start);
+}
+
+static const char *markov_model_get_word(const Markov_model *model, uint32_t word)
+{
+   uint8_t index = (word>>24)&255;
+   word^=(index<<24);
+   return model->words[index].data[word];
+}
+
+static void markov_model_stats(const Markov_model *model)
+{
+   int num_words = 0;
+   for(int i = 0;i<256;i++)
+      num_words+=model->words[i].data_used;
+   printf("%d unique words\n",num_words);
+   printf("%d start words\n",model->starting_words.data_used);
+   int num_contexts = 0;
+   for(int i = 0;i<256;i++)
+      num_contexts+=model->contexts[i].data_used;
+   printf("%d contexts captured\n",num_contexts);
+
+   printf("Word hashmap usage:\n");
+   for(int i = 0;i<256;i++)
+      printf("%d,",model->words[i].data_used);
+   puts("");
+   printf("Context hashmap usage:\n");
+   for(int i = 0;i<256;i++)
+      printf("%d,",model->contexts[i].data_used);
+   puts("");
 }
 
 static void markov_model_generate(const Markov_model *model)
@@ -420,9 +468,14 @@ static void markov_model_generate(const Markov_model *model)
    while(!done)
    {
       int start = sentence.data_used;
+#if MORE_RANDOM
+      int backoff = (rand()%ORDER)+1;
+#else
       int backoff = ORDER;
+#endif
       if(start-backoff<0)
          backoff = start;
+      start--;
 
       uint32_t context[ORDER] = {0};
       for(int i = 0;i<backoff;i++)
@@ -444,15 +497,26 @@ static void markov_model_generate(const Markov_model *model)
 
          model_context = context_find(&model->contexts[xor],context,i);
          if(model_context!=NULL)
-         {
-            printf("Order used: %d\n",i);
             break;
-         }
       }
 
       if(model_context!=NULL)
       {
+#if WEIGHTED_RANDOM
+         uint32_t num = rand()%model_context->total;
+         uint32_t cur = 0;
+         for(int i = 0;i<model_context->counts.data_used;i++)
+         {
+            cur+=model_context->counts.data[i].count;
+            if(cur>=num)
+            {
+               uint32_array_add(&sentence,model_context->counts.data[i].word);
+               break;
+            }
+         }
+#else
          uint32_array_add(&sentence,model_context->counts.data[rand()%model_context->counts.data_used].word);
+#endif
       }
       else
       {
@@ -461,7 +525,7 @@ static void markov_model_generate(const Markov_model *model)
    }
 
    for(int i = 0;i<sentence.data_used;i++)
-      printf("%s ",model->words.data[sentence.data[i]]);
+      printf("%s ",markov_model_get_word(model,sentence.data[i]));
    puts("");
 }
 
@@ -488,5 +552,18 @@ static char *HLH_strtok(char *s, const char *sep, char **context)
    *context = s;
 
    return p;
+}
+
+static uint32_t fnv32a(const char *str)
+{
+   uint32_t hval = 0x811c9dc5;
+   unsigned char *s = (unsigned char *)str;
+   while(*s) 
+   {
+      hval^=(uint32_t)*s++;
+      hval *= FNV_32_PRIME;
+   }
+
+   return hval;
 }
 //-------------------------------------
